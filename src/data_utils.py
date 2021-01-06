3db26c8067a04ed8be756040a361caec66e1c34d
EBolle <ernstbolle@gmail.com>
2021-01-06 11:01:00 +0100

    Added first tests for unique_key_check

--- src/data_utils.py
+++ src/data_utils.py
@@ -7,7 +7,7 @@ import pathlib
 
 def create_csv_path_list(file_path: pathlib.Path) -> list:
     """
-    Returns a list of .csv Paths in the file_path, including all subdirectories.
+    Returns a list of Paths of all files which ends with .csv in the file_path, including all subdirectories.
 
     Example
         from pathlib import Path
@@ -18,7 +18,7 @@ def create_csv_path_list(file_path: pathlib.Path) -> list:
     try:
         return_list = [x for x in file_path.glob("**/*.csv")]
     except AttributeError as err:
-        print(f"There is something wrong with the input, make sure it is a Path like object, error: {err}")
+        print(f"The input is invalid, make sure it is a Path like object, error: {err}")
     else:
         print(f"{file_path} contains {len(return_list)} .csv files.")
         if return_list:
@@ -29,10 +29,19 @@ def unique_key_check(df: pd.DataFrame, keys: list) -> bool:
     """
     Returns TRUE when the combination of keys is unique, these keys can then be used as primary index.
     """
-    if not all([item in df.columns for item in keys]):
-        raise ValueError("The keys are not present in the columns of df, please check again.")
+    if not df.shape[0]:
+        raise ValueError(f"df has {df.shape[0]} rows, please check your input.")
 
-    keys_rows = df.groupby(keys).size().reset_index().shape[0]
+    if not isinstance(keys, list):
+        raise ValueError("The keys parameter must be a list.")
+
+    df.columns = df.columns.str.lower()
+    lower_keys = [key.lower() for key in keys]
+
+    if not all([item in df.columns for item in lower_keys]):
+        raise ValueError("Not all keys are present in the columns of df, please check your input.")
+
+    keys_rows = df.groupby(lower_keys).size().reset_index().shape[0]
     df_rows = df.shape[0]
 
     return keys_rows == df_rows
--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -1,6 +1,8 @@
-# fixture Path list -> returns something? What happens when we insert something else?
 
 
+import csv
+import pandas as pd
+import pathlib
 from pathlib import Path
 import pytest
 from src.data_utils import create_csv_path_list, unique_key_check
@@ -9,28 +11,33 @@ from src.data_utils import create_csv_path_list, unique_key_check
 @pytest.fixture()
 def temp_csv_dir(tmpdir):
     """
-    Temporary directory with .csv files that can be used by several of the tests within this module (fixture).
+    Temporary directory with a single .csv file which includes 1 rows of the actual files used in the project.
+    This directory can be used by several of the tests throughout the project.
     """
     event_data = tmpdir.mkdir('event_data')
     example_csv = event_data.join('example.csv')
-    example_csv.write("""
-    artist,auth,firstName,gender,itemInSession,lastName,length,level,location,method,page,
-    registration,sessionId,song,status,ts,userId
-    """)
-    example_csv.write("""
-    Infected Mushroom,Logged In,Kaylee,F,6,Summers,440.2673,free,"Phoenix-Mesa-Scottsdale,
-    AZ", PUT,NextSong, 1.54034E+12,139,Becoming Insane,200,1.54111E+12,8
-    """)
+
+    with open(example_csv, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['artist', 'auth', 'firstName', 'gender', 'itemInSession', 'lastName', 'length', 'level',
+                         'location', 'method', 'page', 'registration', 'sessionId', 'song', 'status', 'ts', 'userId'])
+        writer.writerow(['Infected Mushroom', 'Logged In', 'Kaylee', 'F', '6', 'Summers', '440.2673', 'free',
+                         "Phoenix-Mesa-Scottsdale, AZ", 'PUT', 'NextSong', '1.54034E+12,139', '139', 'Becoming Insane',
+                         '200', '1.54111E+12', '8'])
 
     return event_data
 
 
-def test_temp_dir(temp_csv_dir):
+def test_csv_list_correct_input(temp_csv_dir):
+    """
+    Tests several characteristics of a valid and non-empty csv_path_list.
+    """
     temp_path = Path(temp_csv_dir)
     csv_path_list = create_csv_path_list(temp_path)
-    print(csv_path_list)
 
     assert len(csv_path_list) == 1
+    assert csv_path_list[0].name == 'example.csv'
+    assert isinstance(csv_path_list[0], (pathlib.WindowsPath, pathlib.PosixPath))
 
 
 @pytest.mark.parametrize('wrong_input', [0, 1.25, 'test', [], (), {}])
@@ -41,23 +48,8 @@ def test_csv_list_wrong_input(wrong_input):
     assert create_csv_path_list(wrong_input) is None
 
 
-def test_csv_list_correct_but_empty_input():
-    """
-    Passing a correct but empty Path like object should only return a print statement, and therefore equal a NoneType.
-    """
-    empty_path = Path('.') / 'tests'
-    empty_path_csv_list = create_csv_path_list(empty_path)
-
-    assert empty_path_csv_list is None
-
-
-def test_csv_list_correct_input():
-    """
-    Passing a correct Path like object with .csv files should return a list of Path like objects.
-    CREATE a temporary directory with fake .csv files -> fixtures -> read?
-    """
-    correct_path = Path('.') / 'event_data'
-    correct_path_csv_list = create_csv_path_list(correct_path)
-
-    assert type(correct_path_csv_list) == list
-    assert len(correct_path_csv_list) > 0
+@pytest.mark.parametrize('wrong_input_keys', [0, 1.25, 'test', (), {}, ('test_1', 'test_2'), {'test': 'test'}])
+def test_unique_key_check_raises(wrong_input_keys):
+    valid_test_df = pd.DataFrame({'test_values': [0, 1, 2, 3]})
+    with pytest.raises(ValueError):
+        unique_key_check(valid_test_df, wrong_input_keys)
